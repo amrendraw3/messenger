9b12aed5e9dc8c88d84f699553166e6fadb8aa99
Amrendra Singh <amrendra.singh@w3villa.com>
2018-03-08 14:18:27 +0530

    Redis pub-sub enabled

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-// app.get('/', function(req, res){
-//   res.send('<h1>Hello world</h1>');
-// });
-
+var redis = require("redis"),
+		subscriber = redis.createClient(),
+  	publisher  = redis.createClient();
+  	
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+
   socket.on('transferdata', function(msg){
-    	console.log('message: ', msg);
-     	io.emit('transferdata', msg);
+  	publisher.publish("transferdata", msg);
+   	io.emit('transferdata', msg);
   });
 });
+
+subscriber.on("message", function(channel, message) {
+  console.log("Message '", message, "' on channel '" + channel + "' arrived!")
+});
+
+subscriber.subscribe("transferdata");
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
