f171c2403c0fd4504e717eb896045ba650309df9
Amrendra Singh <amrendra.singh@w3villa.com>
2018-03-13 17:51:01 +0530

    Handle public and personal events

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ io.on('connection', function(socket){
   
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    for(var uniqueId in clients) {
+      if(clients[uniqueId].socket === socket.id) {
+        console.log('Removing client id: ', uniqueId);
+        delete clients[uniqueId];
+        break;
+      }
+    }
   });
 
   socket.on('add-user', function(data){
@@ -25,16 +32,41 @@ io.on('connection', function(socket){
   });
 
   socket.on('transferdata', function(msg){
-  	publisher.publish("transferdata", msg);
-   	io.emit('transferdata', msg);
+    // Write database logic here and publish event from redis
+  	console.log('msg: ', msg);
+    publisher.publish("transferdata", JSON.stringify(msg));
+  });
+
+  socket.on('personaldata', function(msg){
+    console.log('msg: ', msg);
+    if(!!msg.uniqueId) {
+      publisher.publish("personaldata", JSON.stringify(msg));
+    }
+    // io.emit('transferdata', msg);
   });
 });
 
+// Handle all publish messages on subscribed channel from redis
 subscriber.on("message", function(channel, message) {
-  console.log("Message '", message, "' on channel '" + channel + "' arrived!")
-});
+  console.log("Message '", JSON.parse(message), "' on channel '" + channel + "' arrived!")
+  var message = JSON.parse(message);
+  if(channel === "transferdata") { // public event
+    console.log('Publishing public event!');
+    io.emit('generalData', message);
+  }
+
+  if(channel === "personaldata") { // personal event
+    console.log('TODO: Publishing user-level event!');
+    if (clients[message.uniqueId]){
+      io.sockets.connected[clients[message.uniqueId].socket].emit("personalData", message);
+    } else {
+      console.log("User does not exist: " + message.uniqueId); 
+    }
+  }
+ });
 
 subscriber.subscribe("transferdata");
+subscriber.subscribe("personaldata");
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
